3f8101a64df08391b3b595d61246d59c9e87b478
Savoundararadj <stephan.savoundararadj@consertotech.pro>
2018-05-11 10:45:44 +0200

    final result

---
+++ app/src/main/java/com/example/android/emojify/Emoji.java
@@ -0,0 +1,12 @@
+package com.example.android.emojify;
+
+public enum Emoji {
+    SMILLING,
+    FROWNING,
+    LEFTWINK,
+    RIGHTWINK,
+    RIGHTWINK_FROWNING,
+    LEFTWINK_FROWNING,
+    CLOSED_EYE_SMILLING,
+    CLOSED_EYE_FROWNING
+}
